a595accc60004343a50a137acba1a9fcc37001f4
matsueushi <matsueushi@gmail.com>
2022-06-25 11:38:04 +0000

    256 b

--- abc256/src/bin/b.rs
+++ abc256/src/bin/b.rs
@@ -1,3 +1,22 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        xs: [usize; n]
+    }
+    let mut res = 0;
+    let mut pos = [0; 4];
+    for x in xs {
+        pos[0] += 1;
+        for j in (0..4).rev() {
+            if j + x >= 4 {
+                res += pos[j];
+            } else {
+                pos[j + x] += pos[j];
+            }
+            pos[j] = 0;
+        }
+    }
+    println!("{}", res);
 }
