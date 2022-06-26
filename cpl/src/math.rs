9daf06cee251faeee53148a3a85e8e5749c0fd01
matsueushi <matsueushi@gmail.com>
2022-06-26 13:18:25 +0000

    next_permutation

--- cpl/src/math.rs
+++ cpl/src/math.rs
@@ -1,10 +1,42 @@
-fn next_permutation() {}
+pub fn next_permutation(nums: &mut [i32]) -> bool {
+    let last_asc = match nums.windows(2).rposition(|w| w[0] < w[1]) {
+        None => {
+            nums.reverse();
+            return false;
+        }
+        Some(i) => i,
+    };
+    match nums[last_asc + 1..]
+        .iter()
+        .rposition(|&x| x > nums[last_asc])
+    {
+        None => return false,
+        Some(i) => {
+            nums.swap(last_asc, last_asc + 1 + i);
+            nums[last_asc + 1..].reverse();
+        }
+    }
+    true
+}
 
 #[cfg(test)]
 mod tests {
+    use super::*;
 
     #[test]
     fn test_next_permutation() {
-        assert_eq!(1, 1);
+        let mut nums = vec![1, 2, 3];
+        assert!(next_permutation(&mut nums));
+        assert_eq!(nums, vec![1, 3, 2]);
+        assert!(next_permutation(&mut nums));
+        assert_eq!(nums, vec![2, 1, 3]);
+        assert!(next_permutation(&mut nums));
+        assert_eq!(nums, vec![2, 3, 1]);
+        assert!(next_permutation(&mut nums));
+        assert_eq!(nums, vec![3, 1, 2]);
+        assert!(next_permutation(&mut nums));
+        assert_eq!(nums, vec![3, 2, 1]);
+        assert!(!next_permutation(&mut nums));
+        assert_eq!(nums, vec![1, 2, 3]);
     }
 }
