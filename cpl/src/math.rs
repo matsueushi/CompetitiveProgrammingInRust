c256078eb361aa5d5d59842a2fcf9bba8311bd76
matsueushi <matsueushi@gmail.com>
2022-06-27 14:23:59 +0000

    Prime mod combination

--- cpl/src/math.rs
+++ cpl/src/math.rs
@@ -38,6 +38,39 @@ pub fn pow_mod(x: u64, n: u64, m: u64) -> u64 {
     res
 }
 
+pub struct PrimeModCombination {
+    p: u64,
+    fact: Vec<u64>,
+    fact_inv: Vec<u64>,
+}
+
+impl PrimeModCombination {
+    pub fn new(n: usize, p: u64) -> Self {
+        // i! mod p
+        let mut fact = vec![0; n + 1];
+        // (i!)^{-1} mod p
+        let mut fact_inv = vec![0; n + 1];
+        // i^{-1} mod p (i > 0)
+        let mut iinv = vec![0; n + 1];
+        fact[0] = 1;
+        fact[1] = 1;
+        fact_inv[0] = 1;
+        fact_inv[1] = 1;
+        iinv[1] = 1;
+        for i in 2..n + 1 {
+            let j = i as u64;
+            fact[i] = fact[i - 1] * j % p;
+            iinv[i] = p - iinv[(p % j) as usize] * (p / j) % p;
+            fact_inv[i] = fact_inv[i - 1] * iinv[i] % p;
+        }
+        Self { p, fact, fact_inv }
+    }
+
+    pub fn combination(&self, n: usize, k: usize) -> u64 {
+        self.fact[n] * (self.fact_inv[k] * self.fact_inv[n - k] % self.p) % self.p
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -65,5 +98,12 @@ mod tests {
         assert_eq!(pow_mod(2, 2, 998244353), 4);
         assert_eq!(pow_mod(2, 3, 7), 1);
         assert_eq!(pow_mod(1, 1, 998244353), 1);
+    }
+
+    #[test]
+    fn test_power_mod_combination() {
+        let pmc = PrimeModCombination::new(10, 7);
+        assert_eq!(pmc.combination(3, 2), 3);
+        assert_eq!(pmc.combination(6, 3), 6);
     }
 }
