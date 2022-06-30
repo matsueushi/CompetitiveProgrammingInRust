623952f1159bd4f01dd7f2744c7eab1600d1effc
matsueushi <matsueushi@gmail.com>
2022-06-30 14:12:22 +0000

    fenwick

--- cpl/src/fenwick.rs
+++ cpl/src/fenwick.rs
@@ -1,44 +1,50 @@
-use std::ops::{AddAssign, Sub};
+pub mod fenwick {
 
-pub struct Fenwick<T: Default + Copy + AddAssign + Sub<Output = T>> {
-    n: usize,
-    data: Vec<T>,
-}
+    use std::ops::{AddAssign, Sub};
 
-impl<T: Default + Copy + AddAssign + Sub<Output = T>> Fenwick<T> {
-    fn new(n: usize) -> Self {
-        Self {
-            n,
-            data: vec![T::default(); n],
-        }
+    pub struct Fenwick<T> {
+        n: usize,
+        data: Vec<T>,
     }
 
-    fn add(&mut self, idx: usize, val: T) {
-        let mut idx = idx + 1;
-        while idx <= self.n {
-            self.data[idx - 1] += val;
-            idx += idx & (!idx + 1);
+    impl<T> Fenwick<T>
+    where
+        T: Default + Copy + AddAssign + Sub<Output = T>,
+    {
+        pub fn new(n: usize) -> Self {
+            Self {
+                n,
+                data: vec![T::default(); n],
+            }
         }
-    }
 
-    fn prefix_sum(&self, r: usize) -> T {
-        let mut s = T::default();
-        let mut idx = r;
-        while idx > 0 {
-            s += self.data[idx - 1];
-            idx -= idx & (!idx + 1);
+        pub fn add(&mut self, idx: usize, val: T) {
+            let mut idx = idx + 1;
+            while idx <= self.n {
+                self.data[idx - 1] += val;
+                idx += idx & (!idx + 1);
+            }
         }
-        s
-    }
 
-    fn sum(&self, l: usize, r: usize) -> T {
-        self.prefix_sum(r) - self.prefix_sum(l)
+        pub fn prefix_sum(&self, r: usize) -> T {
+            let mut s = T::default();
+            let mut idx = r;
+            while idx > 0 {
+                s += self.data[idx - 1];
+                idx -= idx & (!idx + 1);
+            }
+            s
+        }
+
+        pub fn sum(&self, l: usize, r: usize) -> T {
+            self.prefix_sum(r) - self.prefix_sum(l)
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use super::fenwick::*;
 
     #[test]
     fn test_fenwick() {
