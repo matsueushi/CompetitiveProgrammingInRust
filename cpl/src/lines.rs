0ac263e9a338210b936b0c043456a3ec418a2405
matsueushi <matsueushi@gmail.com>
2022-08-19 09:34:20 +0000

    generic

--- cpl/src/lines.rs
+++ cpl/src/lines.rs
@@ -1,14 +1,18 @@
 pub mod lines {
     use num::Integer;
+    use std::ops::Neg;
 
-    pub fn normalize_line(x0: i64, y0: i64, x1: i64, y1: i64) -> (i64, i64, i64) {
+    pub fn normalize_line<T>(x0: T, y0: T, x1: T, y1: T) -> (T, T, T)
+    where
+        T: Copy + Integer + Neg<Output = T>,
+    {
         let mut a = y1 - y0;
         let mut b = -(x1 - x0);
         let mut c = x1 * y0 - x0 * y1;
-        if a < 0 || (a == 0 && b < 0) {
-            a *= -1;
-            b *= -1;
-            c *= -1;
+        if a < T::zero() || (a == T::zero() && b < T::zero()) {
+            a = -a;
+            b = -b;
+            c = -c;
         }
         let g = a.gcd(&b);
         (a / g, b / g, c / g)
--- cpl/src/power_mod.rs
+++ cpl/src/power_mod.rs
@@ -1,19 +1,26 @@
 pub mod power_mod {
-    pub fn pow_mod(x: u64, n: u64, m: u64) -> u64 {
-        if n == 0 {
-            return 1;
+    use std::ops::{MulAssign, RemAssign, ShrAssign};
+
+    use num::{PrimInt, Unsigned};
+
+    pub fn pow_mod<T>(x: T, n: T, m: T) -> T
+    where
+        T: PrimInt + Unsigned + MulAssign + RemAssign + ShrAssign,
+    {
+        if n == T::zero() {
+            return T::one();
         }
         let mut x = x % m;
-        let mut res = 1;
+        let mut res = T::one();
         let mut p = n;
-        while p > 0 {
-            if p & 1 != 0 {
+        while p > T::zero() {
+            if p & T::one() != T::zero() {
                 res *= x;
                 res %= m;
             }
             x *= x;
             x %= m;
-            p >>= 1;
+            p >>= T::one();
         }
         res
     }
@@ -25,9 +32,9 @@ mod tests {
 
     #[test]
     fn test_pow_mod() {
-        assert_eq!(pow_mod(5, 3, 19), 11);
-        assert_eq!(pow_mod(2, 2, 998244353), 4);
-        assert_eq!(pow_mod(2, 3, 7), 1);
-        assert_eq!(pow_mod(1, 1, 998244353), 1);
+        assert_eq!(pow_mod::<u32>(5, 3, 19), 11);
+        assert_eq!(pow_mod::<u64>(2, 3, 7), 1);
+        assert_eq!(pow_mod::<usize>(2, 2, 998244353), 4);
+        assert_eq!(pow_mod::<usize>(1, 1, 998244353), 1);
     }
 }
