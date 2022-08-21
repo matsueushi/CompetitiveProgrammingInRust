33a6e56ff2a2b194d008eb3a4693092f4cc1b1d6
matsueushi <matsueushi@gmail.com>
2022-08-21 11:00:36 +0000

    geom

--- cpl/src/geom.rs
+++ cpl/src/geom.rs
@@ -1,4 +1,4 @@
-pub mod lines {
+pub mod geom {
     use num::Integer;
     use std::ops::Neg;
 
@@ -17,16 +17,47 @@ pub mod lines {
         let g = a.gcd(&b);
         (a / g, b / g, c / g)
     }
+
+    pub fn dist2<T>(x0: T, y0: T, x1: T, y1: T) -> T
+    where
+        T: Copy + Integer,
+    {
+        let dx = x0 - x1;
+        let dy = y0 - y1;
+        dx * dx + dy * dy
+    }
+
+    pub fn inner_product<T>(ax: T, ay: T, bx: T, by: T, cx: T, cy: T) -> T
+    where
+        T: Copy + Integer,
+    {
+        let abx = bx - ax;
+        let aby = by - ay;
+        let acx = cx - ax;
+        let acy = cy - ay;
+        abx * acx + aby * acy
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::lines::*;
+    use super::geom::*;
 
     #[test]
     fn test_normalize_line() {
         assert_eq!(normalize_line(1, 1, 2, 2), (1, -1, 0));
         assert_eq!(normalize_line(1, 0, 2, 0), (0, 1, 0));
         assert_eq!(normalize_line(0, 1, 0, 2), (1, 0, 0));
+    }
+
+    #[test]
+    fn test_dist2() {
+        assert_eq!(dist2(1, 1, -1, -1), 8);
+    }
+
+    #[test]
+    fn test_inner_product() {
+        assert_eq!(inner_product(0, 0, 2, -1, 1, 2), 0);
+        assert_eq!(inner_product(0, 0, 5, 0, 2, 4), 10);
     }
 }
