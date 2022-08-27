522025d3d30da37145fe6a6d44d7e56241aa2c10
matsueushi <matsueushi@gmail.com>
2022-08-27 00:26:47 +0000

    065

--- math-and-algorithm/src/bin/065.rs
+++ math-and-algorithm/src/bin/065.rs
@@ -6,6 +6,10 @@ fn main() {
         h: usize,
         w: usize,
     }
+    if h == 1 || w == 1 {
+        println!("1");
+        return;
+    }
     let c = w / 2;
     let res = h * c + (w % 2) * ((h + 1) / 2);
     println!("{}", res);
