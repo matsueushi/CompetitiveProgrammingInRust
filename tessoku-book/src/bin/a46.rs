c12d236f51053fb0c6cd1d8692f8c9059fe6df0e
matsueushi <matsueushi@gmail.com>
2023-01-15 06:27:16 +0000

    greedy

--- tessoku-book/src/bin/a46.rs
+++ tessoku-book/src/bin/a46.rs
@@ -1,48 +1,63 @@
-use proconio::{fastout, input};
+use proconio::input;
 
-fn distance(u: &(i64, i64), v: &(i64, i64)) -> f64 {
-    let (ux, uy) = u;
-    let (vx, vy) = v;
-    let dx = ux - vx;
-    let dy = uy - vy;
+#[derive(Debug)]
+struct Point {
+    x: i64,
+    y: i64,
+}
+
+fn get_distance(a: &Point, b: &Point) -> f64 {
+    let dx = a.x - b.x;
+    let dy = a.y - b.y;
     ((dx * dx + dy * dy) as f64).sqrt()
 }
 
-#[fastout]
-fn main() {
-    input! {
-        n: usize,
-        xy: [(i64,i64); n]
-    }
-    let mut ord = vec![0; n];
+fn play_greedy(n: usize, points: Vec<Point>) -> Vec<usize> {
+    let mut order = vec![0; n];
+
+    let mut current_place = 1;
     let mut visited = vec![false; n];
 
-    let mut curpos = 0;
-    ord[0] = 0;
     visited[0] = true;
 
+    // 貪欲法スタート
     for i in 1..n {
-        let mut min_d = 10000.0;
-        let mut min_id = 0;
+        let mut min_dist = 10000.00; // 現時点での距離の最小
+        let mut min_id = 0; // 次はどの年に移動すれば良いか
 
+        // 距離が最小となる都市を探す
         for j in 0..n {
             if visited[j] {
                 continue;
             }
-            let d = distance(&xy[curpos], &xy[j]);
-            if d < min_d {
-                min_d = d;
+            let new_dist = get_distance(&points[current_place], &points[j]);
+            if min_dist > new_dist {
+                min_dist = new_dist;
                 min_id = j;
             }
         }
 
-        ord[i] = min_id;
+        // 現在位置の更新
         visited[min_id] = true;
-        curpos = min_id;
+        order[i] = min_id;
+        current_place = min_id;
     }
 
-    for x in ord {
-        println!("{}", x + 1);
+    order
+}
+
+fn main() {
+    input! {
+        n: usize,
+        xys: [(i64, i64); n],
     }
-    println!("{}", 1);
+    let mut points = Vec::new();
+    for (x, y) in xys {
+        points.push(Point { x, y });
+    }
+    let order = play_greedy(n, points);
+    for i in order {
+        println!("{}", i + 1);
+    }
+    println!("1");
 }
