51a46adb1cdae2baac1fcf6021fbea39893b039d
matsueushi <matsueushi@gmail.com>
2023-02-23 13:55:45 +0000

    unionfind

--- ahc018/src/main.rs
+++ ahc018/src/main.rs
@@ -3,10 +3,76 @@ use std::io::BufReader;
 use std::process;
 use text_io::read;
 
+#[derive(Debug, Clone)]
+pub struct UnionFind {
+    par: Vec<usize>,
+    size: Vec<usize>,
+}
+
+impl UnionFind {
+    /// 大きさ `n` の Union-Find 木を初期化する。
+    pub fn new(n: usize) -> Self {
+        Self {
+            par: vec![0; n],
+            size: vec![1; n],
+        }
+    }
+
+    /// 頂点 `a` の属する連結成分の代表元を返す。
+    pub fn find_root(&mut self, a: usize) -> usize {
+        if self.size[a] > 0 {
+            return a;
+        }
+        self.par[a] = self.find_root(self.par[a]);
+        self.par[a]
+    }
+
+    /// 辺 `(a, b)` を追加し、追加後の連結成分の代表元を返す。
+    pub fn union(&mut self, a: usize, b: usize) -> usize {
+        let mut x = self.find_root(a);
+        let mut y = self.find_root(b);
+        if x == y {
+            return x;
+        }
+        if self.size[x] < self.size[y] {
+            std::mem::swap(&mut x, &mut y);
+        }
+        self.size[x] += self.size[y];
+        self.size[y] = 0;
+        self.par[y] = x;
+        x
+    }
+
+    /// 頂点 `a` と 頂点 `b` が同じ連結成分に属しているかを返す。
+    pub fn in_same_set(&mut self, a: usize, b: usize) -> bool {
+        self.find_root(a) == self.find_root(b)
+    }
+
+    /// 頂点 `a` の属する連結成分のサイズを返す。
+    pub fn group_size(&mut self, a: usize) -> usize {
+        let x = self.find_root(a);
+        self.size[x]
+    }
+}
+
 #[derive(Clone, Copy)]
 struct Pos {
     y: usize,
     x: usize,
+}
+
+fn abs_diff(a: usize, b: usize) -> usize {
+    if a > b {
+        a - b
+    } else {
+        b - a
+    }
+}
+
+impl Pos {
+    pub fn dist(&self, other: &Pos) -> usize {
+        abs_diff(self.x, other.x) + abs_diff(self.y, other.y)
+    }
 }
 
 enum Response {
@@ -75,8 +141,49 @@ impl Solver {
     }
 
     pub fn solve(&mut self) {
-        for house in self.house_pos.clone() {
-            self.mov(house, self.source_pos[0]);
+        let hl = self.house_pos.len();
+        let mut uf = UnionFind::new(hl + 1);
+
+        while uf.group_size(hl) != hl + 1 {
+            for i in 0..hl {
+                // すでに水源に接続されている
+                if uf.in_same_set(i, hl) {
+                    continue;
+                }
+
+                // 接続されていない
+                let mut hidx = 0;
+                let mut hdist = std::usize::MAX;
+                for j in 0..hl {
+                    if uf.in_same_set(i, j) {
+                        continue;
+                    }
+                    let d = self.house_pos[i].dist(&self.house_pos[j]);
+                    if d < hdist {
+                        hdist = d;
+                        hidx = j;
+                    }
+                }
+
+                let mut sidx = 0;
+                let mut sdist = std::usize::MAX;
+                for j in 0..self.source_pos.len() {
+                    let d = self.house_pos[i].dist(&self.source_pos[j]);
+                    if d < sdist {
+                        sdist = d;
+                        sidx = j;
+                    }
+                }
+
+                if sdist < hdist {
+                    // 水源に繋いだ方が近い
+                    self.mov(self.house_pos[i], self.source_pos[sidx]);
+                    uf.union(i, hl);
+                } else {
+                    self.mov(self.house_pos[i], self.house_pos[hidx]);
+                    uf.union(i, hidx);
+                }
+            }
         }
     }
 
