285b4a681c82caf5517315f1bbad1fac71415892
matsueushi <matsueushi@gmail.com>
2023-03-04 02:44:11 +0000

    整理

---
+++ ahc018/copy.sh
@@ -0,0 +1,2 @@
+cp tools/score.txt tools/score_best.txt
+cp tools/out/* tools/out_best/*
--- ahc018/src/main.rs
+++ ahc018/src/main.rs
@@ -1,7 +1,8 @@
-use petgraph::unionfind::UnionFind;
 use proconio::{input, source::line::LineSource};
 use std::io::BufReader;
 use std::process;
+#[cfg(feature = "local")]
+use svg::node::element::{path::Data, Path};
 use text_io::read;
 
 /// 場所を示す構造体。
@@ -28,14 +29,18 @@ enum Response {
 
 /// フィールド
 struct Field {
+    source_pos: Vec<Pos>,
+    house_pos: Vec<Pos>,
     power: Vec<Vec<usize>>,    // 掘削するのに消費したパワー
     n_trial: Vec<Vec<usize>>,  // 掘削した回数
     is_broken: Vec<Vec<bool>>, // すでにフィールドが壊れているか
 }
 
 impl Field {
-    pub fn new(n: usize, c: usize) -> Self {
+    pub fn new(n: usize, source_pos: Vec<Pos>, house_pos: Vec<Pos>) -> Self {
         Self {
+            source_pos,
+            house_pos,
             power: vec![vec![0; n]; n],
             n_trial: vec![vec![0; n]; n],
             is_broken: vec![vec![false; n]; n],
@@ -74,8 +79,6 @@ struct Solver {
     w: usize, // 水源の数、1 <= w <= 4
     k: usize, // 家の数、 1 <= k <= 10
     c: usize, // 体力の消費, c in 1,2,4,8,16,32,64,128
-    source_pos: Vec<Pos>,
-    house_pos: Vec<Pos>,
     field: Field,
 }
 
@@ -88,21 +91,13 @@ impl Solver {
         source_pos: Vec<Pos>,
         house_pos: Vec<Pos>,
     ) -> Self {
-        let field = Field::new(n, c);
-        Self {
-            n,
-            w,
-            k,
-            c,
-            source_pos,
-            house_pos,
-            field,
-        }
+        let field = Field::new(n, source_pos, house_pos);
+        Self { n, w, k, c, field }
     }
 
     pub fn solve(&mut self) {
-        for house in self.house_pos.clone() {
-            self.walk(house, self.source_pos[0]);
+        for house in self.field.house_pos.clone() {
+            self.walk(house, self.field.source_pos[0]);
         }
     }
 
@@ -155,19 +150,7 @@ impl Solver {
             }
         }
     }
-
-    // 試掘を行う。
-    // pub fn boring(&self) {
-    //     visualize_boring()
-    // }
 }
-
-/// ボーリングを行うボーリングミル
-// struct BoringMill {
-//     cost: Vec<Vec<usize>>,
-// }
-
-// impl BoringMill {}
 
 /// メイン関数
 /// ../target/release/tester cargo run < tools/in/0000.txt > tools/out/0000.txt
@@ -195,47 +178,43 @@ fn main() {
     }
 
     let mut solver = Solver::new(n, w, k, c, source_pos, house_pos);
-    // solver.boring();
     solver.solve();
 }
 
 ///
 /// 以下、考察用
-///
-#[cfg(feature = "local")]
-use svg::node::element::{path::Data, Path};
 
 #[cfg(not(feature = "local"))]
 #[allow(dead_code)]
 fn visualize_boring() {}
 
-#[cfg(feature = "local")]
-fn create_rect_data(y0: usize, x0: usize, y1: usize, x1: usize) -> Data {
-    Data::new()
-        .move_to((x0, y0))
-        .line_by((x1 as i64 - x0 as i64, 0))
-        .line_by((0, y1 as i64 - y0 as i64))
-        .line_by((x0 as i64 - x1 as i64, 0))
-        .close()
-}
+// #[cfg(feature = "local")]
+// fn create_rect_data(y0: usize, x0: usize, y1: usize, x1: usize) -> Data {
+//     Data::new()
+//         .move_to((x0, y0))
+//         .line_by((x1 as i64 - x0 as i64, 0))
+//         .line_by((0, y1 as i64 - y0 as i64))
+//         .line_by((x0 as i64 - x1 as i64, 0))
+//         .close()
+// }
 
-#[cfg(feature = "local")]
-#[allow(dead_code)]
-fn visualize_boring() {
-    let margin = 20;
-    let w = 200;
-    let d = w + 2 * margin;
-    let mut doc = svg::Document::new().set("viewBox", (0, 0, d, d));
-    let back = Path::new().set("fill", "white").set(
-        "d",
-        create_rect_data(margin, margin, margin + w, margin + w),
-    );
-    doc = doc.add(back);
-    let text = svg::node::element::Text::new()
-        .set("x", margin)
-        .set("y", margin)
-        .set("font-size", 10)
-        .add(svg::node::Text::new("aaa"));
-    doc = doc.add(text);
-    svg::save("boring.svg", &doc).unwrap();
-}
+// #[cfg(feature = "local")]
+// #[allow(dead_code)]
+// fn visualize_boring() {
+//     let margin = 20;
+//     let w = 200;
+//     let d = w + 2 * margin;
+//     let mut doc = svg::Document::new().set("viewBox", (0, 0, d, d));
+//     let back = Path::new().set("fill", "white").set(
+//         "d",
+//         create_rect_data(margin, margin, margin + w, margin + w),
+//     );
+//     doc = doc.add(back);
+//     let text = svg::node::element::Text::new()
+//         .set("x", margin)
+//         .set("y", margin)
+//         .set("font-size", 10)
+//         .add(svg::node::Text::new("aaa"));
+//     doc = doc.add(text);
+//     svg::save("boring.svg", &doc).unwrap();
+// }
