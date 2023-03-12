23d8a969cfc4ce1604c591548cb75efe87352785
matsueushi <matsueushi@gmail.com>
2023-03-12 14:31:28 +0000

    println

--- toyota-hc-2023spring/src/bin/a.rs
+++ toyota-hc-2023spring/src/bin/a.rs
@@ -2,6 +2,7 @@
 
 use rand::seq::SliceRandom;
 use rand::{Rng, SeedableRng};
+use std::fmt;
 use std::fmt::format;
 use std::fs::{create_dir_all, File};
 use std::io::{BufWriter, Write};
@@ -14,7 +15,7 @@ use proconio::input;
 /// 荷物
 #[derive(Debug, Clone, Copy, Eq, PartialEq)]
 struct Cargo {
-    num: usize,
+    p: usize,
     w: usize,
     h: usize,
     d: usize,
@@ -24,9 +25,9 @@ struct Cargo {
 }
 
 impl Cargo {
-    pub fn new(num: usize, w: usize, h: usize, d: usize, this_side: bool, fragile: bool) -> Self {
+    pub fn new(p: usize, w: usize, h: usize, d: usize, this_side: bool, fragile: bool) -> Self {
         Self {
-            num,
+            p,
             w,
             h,
             d,
@@ -226,41 +227,78 @@ struct Container {
 }
 
 impl Container {
-    pub fn new(w: usize, h: usize, b: usize, d: usize) -> Self {
+    pub fn new(w: usize, h: usize, b: usize, d: usize, seed: usize) -> Self {
         let mut objs = HashMap::new();
-        objs.insert(
-            0,
-            Plate {
-                x: 0,
-                y: b,
-                z: 0,
-                w,
-                h: h - 2 * b,
-                fragile: false,
-            },
-        );
-        objs.insert(
-            1,
-            Plate {
-                x: b,
-                y: 0,
-                z: 0,
-                w: w - 2 * b,
-                h: b,
-                fragile: false,
-            },
-        );
-        objs.insert(
-            2,
-            Plate {
-                x: b,
-                y: h - b,
-                z: 0,
-                w: w - 2 * b,
-                h: b,
-                fragile: false,
-            },
-        );
+        if seed % 2 == 0 {
+            objs.insert(
+                0,
+                Plate {
+                    x: 0,
+                    y: b,
+                    z: 0,
+                    w,
+                    h: h - 2 * b,
+                    fragile: false,
+                },
+            );
+            objs.insert(
+                1,
+                Plate {
+                    x: b,
+                    y: 0,
+                    z: 0,
+                    w: w - 2 * b,
+                    h: b,
+                    fragile: false,
+                },
+            );
+            objs.insert(
+                2,
+                Plate {
+                    x: b,
+                    y: h - b,
+                    z: 0,
+                    w: w - 2 * b,
+                    h: b,
+                    fragile: false,
+                },
+            );
+        } else {
+            objs.insert(
+                0,
+                Plate {
+                    x: b,
+                    y: 0,
+                    z: 0,
+                    w: w - 2 * b,
+                    h,
+                    fragile: false,
+                },
+            );
+            objs.insert(
+                1,
+                Plate {
+                    x: 0,
+                    y: d,
+                    z: 0,
+                    w: b,
+                    h: h - 2 * b,
+                    fragile: false,
+                },
+            );
+            objs.insert(
+                2,
+                Plate {
+                    x: w - b,
+                    y: b,
+                    z: 0,
+                    w: b,
+                    h: h - 2 * b,
+                    fragile: false,
+                },
+            );
+        }
+
         Self {
             w,
             h,
@@ -320,6 +358,37 @@ impl Container {
     }
 }
 
+type Pos = (usize, usize, usize);
+
+struct Solution {
+    positions: Vec<Pos>,
+    cargos: Vec<Cargo>,
+}
+
+impl Solution {
+    pub fn new() -> Self {
+        Self {
+            positions: Vec::new(),
+            cargos: Vec::new(),
+        }
+    }
+
+    pub fn add(&mut self, pos: Pos, cargo: Cargo) {
+        self.positions.push(pos);
+        self.cargos.push(cargo);
+    }
+
+    pub fn print_result(&self) {
+        for i in 0..self.positions.len() {
+            let (x, y, z) = self.positions[i];
+            println!(
+                "{} {} {} {} {}",
+                self.cargos[i].p, self.cargos[i].state, x, y, z
+            )
+        }
+    }
+}
+
 struct Solver {
     w: usize,
     h: usize,
@@ -328,12 +397,14 @@ struct Solver {
     cargos: Vec<Cargo>,
 }
 
-type Solution = Vec<(usize, usize, usize, usize, usize)>;
-
 impl Solver {
-    pub fn solve_for_cargo(&self, mut cargos: &mut Vec<Cargo>) -> Result<Solution, Solution> {
-        let mut container = Container::new(self.w, self.h, self.b, self.d);
-        let mut solution = Vec::new();
+    pub fn solve_for_cargo(
+        &self,
+        mut cargos: &mut Vec<Cargo>,
+        seed: usize,
+    ) -> Result<Solution, Solution> {
+        let mut container = Container::new(self.w, self.h, self.b, self.d, seed);
+        let mut solution = Solution::new();
         for cargo in cargos {
             let mut cost = std::usize::MAX;
             let mut found = false;
@@ -360,8 +431,8 @@ impl Solver {
 
             if found {
                 cargo.set_state(best_state);
-                let (x, y, z) = container.plate_position(best_id);
-                solution.push((cargo.num, best_state, x, y, z));
+                let pos = container.plate_position(best_id);
+                solution.add(pos, *cargo);
                 container.allocate(best_id, best_plates);
             } else {
                 // eprintln!("error: {:?}", cargo);
@@ -378,9 +449,10 @@ impl Solver {
 
         let mut rng = rand_chacha::ChaCha8Rng::seed_from_u64(42);
         let mut cargos = self.cargos.clone();
-        let mut sol = Vec::new();
+        let mut sol = Solution::new();
         while since.elapsed().as_secs_f32() < 1.8 {
-            match self.solve_for_cargo(&mut cargos) {
+            let seed = rng.gen_range(0, 2);
+            match self.solve_for_cargo(&mut cargos, seed) {
                 Ok(solution) => return solution,
                 Err(solution) => {
                     // eprintln!("Solution not found");
@@ -407,17 +479,15 @@ fn main() {
     // eprintln!("{:?}", cgs);
 
     let mut cargos = Vec::new();
-    for (num, (h, w, d, a, f, g)) in enumerate(cgs) {
+    for (p, (h, w, d, a, f, g)) in enumerate(cgs) {
         for _ in 0..a {
-            cargos.push(Cargo::new(num, w, h, d, f != "Y", g != "Y"));
+            cargos.push(Cargo::new(p, w, h, d, f != "Y", g != "Y"));
         }
     }
 
     let mut solver = Solver { w, h, b, d, cargos };
     let solution = solver.solve();
-    for (p, r, x, y, z) in solution {
-        println!("{} {} {} {} {}", p, r, x, y, z);
-    }
+    solution.print_result();
 }
 
 // cargo run < tools/in/0000.txt > tools/out/0000.txt
