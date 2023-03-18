232c954b32278ed6f3c62964e30493c30665b2ea
matsueushi <matsueushi@gmail.com>
2023-03-18 07:42:37 +0000

    working...

--- toyota-hc-2023spring/src/bin/a.rs
+++ toyota-hc-2023spring/src/bin/a.rs
@@ -1,14 +1,26 @@
 #![allow(unused)]
 
-const CORNER: usize = 10000;
+const BLOCK: usize = 10000;
 
+use itertools::enumerate;
 use proconio::input;
+use std::cmp::Ordering;
 
 #[derive(Debug, Clone, Copy, Eq, PartialEq)]
 struct Vector3D {
     x: usize,
     y: usize,
     z: usize,
+}
+
+impl Vector3D {
+    fn slide(&self, x: usize, y: usize, z: usize) -> Self {
+        Self {
+            x: self.x + x,
+            y: self.y + y,
+            z: self.z + z,
+        }
+    }
 }
 
 type Space = Vector3D;
@@ -49,11 +61,23 @@ struct Item {
     h: usize,
     d: usize,
     orientation: usize,
-    fragile: bool,
     flippable: bool,
+    fragile: bool,
 }
 
 impl Item {
+    fn new_block(w: usize, h: usize, d: usize) -> Self {
+        Self {
+            id: BLOCK,
+            w,
+            h,
+            d,
+            orientation: 0,
+            flippable: true,
+            fragile: false,
+        }
+    }
+
     fn volume(&self) -> usize {
         self.w * self.h * self.d
     }
@@ -68,8 +92,8 @@ impl Item {
                 h: self.h,
                 d: self.d,
                 orientation: self.orientation + 1,
-                fragile: self.fragile,
                 flippable: self.flippable,
+                fragile: self.fragile,
             })
         }
     }
@@ -107,31 +131,34 @@ impl Item {
         }
     }
 
-    fn project_x(&self) -> Rect {
+    fn project_x(&self, pos: &Position) -> Rect {
         Rect {
             x0: 0,
             y0: 0,
             x1: self.dim_y(),
             y1: self.dim_z(),
         }
+        .slide(pos.y, pos.z)
     }
 
-    fn project_y(&self) -> Rect {
+    fn project_y(&self, pos: &Position) -> Rect {
         Rect {
             x0: 0,
             y0: 0,
             x1: self.dim_x(),
             y1: self.dim_z(),
         }
+        .slide(pos.x, pos.z)
     }
 
-    fn project_z(&self) -> Rect {
+    fn project_z(&self, pos: &Position) -> Rect {
         Rect {
             x0: 0,
             y0: 0,
             x1: self.dim_x(),
             y1: self.dim_y(),
         }
+        .slide(pos.x, pos.y)
     }
 
     fn space(&self) -> Space {
@@ -143,20 +170,116 @@ impl Item {
     }
 }
 
+impl Ord for Item {
+    fn cmp(&self, other: &Self) -> Ordering {
+        (!self.fragile, self.volume())
+            .cmp(&(!other.fragile, other.volume()))
+            .reverse()
+    }
+}
+
+impl PartialOrd for Item {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+#[derive(Debug)]
 struct Placement {
     pos: Position,
     item: Item,
 }
 
 impl Placement {
-    // 長方形の交差判定をする
+    fn project_x(&self) -> Rect {
+        self.item.project_x(&self.pos)
+    }
 
+    fn project_y(&self) -> Rect {
+        self.item.project_y(&self.pos)
+    }
+
+    fn project_z(&self) -> Rect {
+        self.item.project_z(&self.pos)
+    }
+
+    /// 長方形の交差判定をする
     fn intersect(&self, other: &Self) -> bool {
-        false
+        self.project_x().intersect(&other.project_x())
+            || self.project_y().intersect(&other.project_y())
+            || self.project_z().intersect(&other.project_z())
+    }
+
+    fn vertices(&self) -> Vec<Position> {
+        let proj_z = self.project_z();
+        let mut vs = Vec::new();
+        vs.push(self.pos.slide(self.item.dim_x(), 0, 0));
+        vs.push(self.pos.slide(0, self.item.dim_y(), 0));
+        vs
+    }
+
+    fn print(&self) {
+        if self.item.id == BLOCK {
+            return;
+        }
+        println!(
+            "{} {} {} {} {}",
+            self.item.id, self.item.orientation, self.pos.x, self.pos.y, self.pos.z
+        );
     }
 }
 
-struct Packer {}
+#[derive(Debug)]
+struct Packer {
+    w: usize,
+    h: usize,
+    d: usize,
+    packed: Vec<Placement>,
+    vertices: Vec<Position>,
+}
+
+impl Packer {
+    fn new(w: usize, h: usize, d: usize, b: usize) -> Self {
+        let mut packer = Self {
+            w,
+            h,
+            d,
+            packed: Vec::new(),
+            vertices: Vec::new(),
+        };
+        packer.put_item(packer.block(0, 0, b));
+        packer.put_item(packer.block(w - b, 0, b));
+        packer.put_item(packer.block(0, h - b, b));
+        packer.put_item(packer.block(w - b, h - b, b));
+        packer
+    }
+
+    fn block(&self, x: usize, y: usize, b: usize) -> Placement {
+        Placement {
+            pos: Position { x, y, z: 0 },
+            item: Item::new_block(b, b, self.d),
+        }
+    }
+
+    fn put_item(&mut self, placement: Placement) {
+        placement.print();
+
+        let mut vs = placement.vertices();
+        self.packed.push(placement);
+        for v in vs {
+            if v.x == self.w || v.y == self.h {
+                continue;
+            }
+            self.vertices.push(v);
+        }
+    }
+
+    fn pack(&self, items: Vec<Item>) {
+        let mut items = items;
+        items.sort();
+        eprintln!("{:?}", items);
+    }
+}
 
 fn main() {
     input! {
@@ -168,7 +291,24 @@ fn main() {
         cs: [(usize, usize, usize, usize, String, String); m]
     }
 
-    let packer = Packer {};
+    let mut items = Vec::new();
+    for (id, (hi, wi, di, ai, fi, gi)) in enumerate(cs) {
+        for _ in 0..ai {
+            items.push(Item {
+                id,
+                w: wi,
+                h: hi,
+                d: di,
+                orientation: 0,
+                flippable: fi == "Y",
+                fragile: gi == "N",
+            })
+        }
+    }
+
+    let packer = Packer::new(w, h, d, b);
+    packer.pack(items);
+    // eprintln!("{:?}", packer);
 }
 
 ///
